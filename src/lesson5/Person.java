e8e934c2c29e34cde0d9ab515538dcfe51cc321d
Poupeye <106345339+Poupeye@users.noreply.github.com>
2022-06-04 14:27:01 +0300

    less 5

--- src/lesson5/Person.java
+++ src/lesson5/Person.java
@@ -1,12 +1,12 @@
 package lesson5;
 
 public class Person {
-    public String name;
-    public String jobTitle;
-    public String email;
-    public int tell;
-    public int money;
-    public int age;
+    private String name;
+    private String jobTitle;
+    private String email;
+    private int tell;
+    private int money;
+    private int age;
 
     public Person(String name, String jobTitle, String email, int tell, int money, int age) {
         this.name = name;
@@ -17,7 +17,55 @@ public class Person {
         this.age = age;
     }
 
-    public void info() {
+    public void  info() {
         System.out.println("Сотрудник-" + name + "\t" + "Возраст-" + age + "\t" + "Должность-" + jobTitle + "\t" + "Email-" + email + "\t" + "Телефон-" + tell + "\t" + "Затплата-" + money);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTell() {
+        return tell;
+    }
+
+    public void setTell(int tell) {
+        this.tell = tell;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
